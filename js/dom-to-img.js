d9783dd22675a2ff310ef215b90ce2556ac1cc95
jmtes <juliennetesoro@gmail.com>
2020-12-22 23:15:29 -0800

    Clone element inside iframe

--- js/dom-to-img.js
+++ js/dom-to-img.js
@@ -1,15 +1,15 @@
-import domToImage from 'dom-to-image';
-
-const outputDiv = document.getElementById('dom-to-img-output');
+const outputFrame = document.getElementById('output').contentWindow.document;
+const outputFrameHead = outputFrame.head;
+const outputFrameBody = outputFrame.body;
 
 const tellMeMore = document.getElementById('tell-me-more');
 const ecommerce = document.getElementById('e-commerce');
 
-const displayImage = (dataUrl) => {
-  const img = new Image();
-  img.src = dataUrl;
-  outputDiv.appendChild(img);
-};
+const importedHead = document.importNode(document.querySelector('head'), true);
+const importedButton = document.importNode(tellMeMore, true);
 
-domToImage.toPng(tellMeMore).then(displayImage);
-domToImage.toPng(ecommerce).then(displayImage);
+outputFrameBody.append(importedButton);
+
+console.log(importedHead);
+
+outputFrameHead.replaceWith(importedHead);
