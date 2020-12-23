ea4284a04d21462ef4af720c93fe126856432b14
jmtes <juliennetesoro@gmail.com>
2020-12-22 22:11:09 -0800

    Test out dom-to-image

--- js/dom-to-img.js
+++ js/dom-to-img.js
@@ -1,1 +1,15 @@
+import domToImage from 'dom-to-image';
 
+const outputDiv = document.getElementById('dom-to-img-output');
+
+const tellMeMore = document.getElementById('tell-me-more');
+const ecommerce = document.getElementById('e-commerce');
+
+const displayImage = (dataUrl) => {
+  const img = new Image();
+  img.src = dataUrl;
+  outputDiv.appendChild(img);
+};
+
+domToImage.toPng(tellMeMore).then(displayImage);
+domToImage.toPng(ecommerce).then(displayImage);
